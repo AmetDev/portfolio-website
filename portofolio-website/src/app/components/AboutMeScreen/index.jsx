5861272ed6cbbf9ae849469e54f3b3d89120e9fe
AmetDev <ametsejdaliev@gmail.com>
2024-01-12 15:30:42 +0300

    fix-position-for-about-me-screen

--- portofolio-website/src/app/components/AboutMeScreen/index.jsx
+++ portofolio-website/src/app/components/AboutMeScreen/index.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import styles from './AboutMeScreen.module.scss'
+import img from '@public/icons/pixel.png'
+import Image from 'next/image';
 const AboutMeScreen = () => {
     return (
-        <div className={styles.aboutMeWrapper}>
-            <h2>Обо мне</h2>
-            hhhh
-        </div>
+        <div className={styles.aboutMeWrapper}><h2>Обо мне</h2>
+
+          <div>       <Image src={img}/>
+        <h1>Full-stack разработчик в WEB & Mobile Разработке</h1>
+</div>
+                            </div>
     );
 };
 
